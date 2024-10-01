28f8d501ecfa44ffe00fef900d61ad8bb2b0de51
Andrew Dieken <andrewrd@live.com>
2024-09-30 21:41:39 -0700

    refactor(main.py): create functions and add main method

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-import os
+import argparse
 import json
+import os
 
-from elasticsearch import Elasticsearch
 from dotenv import load_dotenv
-
+from elasticsearch import Elasticsearch
 
 load_dotenv(dotenv_path='.env')
 
 es_client = Elasticsearch(
     os.getenv("ELASTICSEARCH_HOST"),
-    api_key=(os.getenv("ELASTICSEARCH_API_ID"), os.getenv("ELASTICSEARCH_API_KEY"))
+    api_key=(os.getenv("ELASTICSEARCH_API_KEY_ID"), os.getenv("ELASTICSEARCH_API_KEY"))
 )
 
-# Create the 'account' index
-account_index_name = 'account'
-settings = json.load(open('index_settings.json'))
-mappings = json.load(open('index_mappings.json'))
-es_client.indices.create(index=account_index_name, settings=settings, mappings=mappings)
+index_name = 'account'
 
-# Populate the 'account' index with some data
-accounts = json.load(open('index_data.json'))
 
+def create_index():
+    """
+    Create the 'account' index.
+    """
+    settings = json.load(open('index_settings.json'))
+    mappings = json.load(open('index_mappings.json'))
+    es_client.indices.create(index=index_name, settings=settings, mappings=mappings)
+
+
+def _delete_index():
+    """
+    Delete the 'account' index.
+    """
+    es_client.indices.delete(index=index_name)
+
+
+def populate_index():
+    """
+    Populate the 'account' index with data.
+    """
+    accounts = json.load(open('index_data.json'))
+    for account in accounts:
+        es_client.index(index=index_name, body=account)
+
+
+if __name__ == 'main':
+    parser = argparse.ArgumentParser(description='Manage Elasticsearch index.')
+    parser.add_argument('--create', action='store_true', help='Create the index')
+    parser.add_argument('--populate', action='store_true', help='Populate the index with data')
+    args = parser.parse_args()
+
+    if args.create:
+        create_index()
+
+    if args.populate:
+        populate_index()
