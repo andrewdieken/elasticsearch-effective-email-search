1ad3969e78a03330385a1c0ed01351aab4397787
Andrew Dieken <andrewrd@live.com>
2024-10-14 10:16:56 -0700

    refactor(main.py): fix main and add helpful print statements

--- main.py
+++ main.py
@@ -23,28 +23,37 @@ def create_index():
         print(f"Index '{index_name}' already exists. If you'd like to recreate the index, delete the existing index first with the 'delete_index' flag.")
         return
 
+    print(f"Creating index '{index_name}'...")
     settings = json.load(open('index_settings.json'))
     mappings = json.load(open('index_mappings.json'))
     es_client.indices.create(index=index_name, settings=settings, mappings=mappings)
+    print(f"Index '{index_name}' created successfully.")
 
 
 def delete_index():
     """
     Delete the 'account' index.
     """
+    if not es_client.indices.exists(index=index_name):
+        print(f"Unable to delete, index '{index_name}' does not exist.")
+        return
+
+    print(f"Deleting index '{index_name}'...")
     es_client.indices.delete(index=index_name)
+    print(f"{index_name}' deleted successfully.")
 
 
 def populate_index():
     """
     Populate the 'account' index with data.
     """
+    print(f"Populating index '{index_name}'...")
     accounts = json.load(open('index_data.json'))
     for account in accounts:
         es_client.index(index=index_name, body=account)
+    print(f"Index '{index_name}' populated successfully.")
 
-
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Manage Elasticsearch index.')
     parser.add_argument('--create', action='store_true', help='Create the index')
     parser.add_argument('--delete', action='store_true', help='Delete the index')
